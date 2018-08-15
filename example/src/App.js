8664043e409bc5cde594e1dd6a2ab0822fd47f81
oahmaro <osamaahmaro@gmail.com>
2018-08-16 02:20:45 +0300

    feat: Create SplashScreen Component

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-
-import ExampleComponent from 'rhinoui'
+import { SplashScreen } from 'rhinoui'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <SplashScreen 
+          theme='dark'
+          borderRadius={15}
+          style={{'margin': 0, 'position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)'}}
+          size={300}
+          img='https://placeimg.com/300/300/any/grayscale' 
+          title='Create your first contact!' 
+          buttonText='Create new contact...'/>
       </div>
     )
   }
---
+++ src/components/SplashScreen/SplashScreen.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.css'
+import classnames from 'classnames'
+
+class SplashScreen extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    img: PropTypes.string,
+    buttonText: PropTypes.string,
+    size: PropTypes.number,
+    style: PropTypes.object,
+    borderRadius: PropTypes.number,
+    theme: PropTypes.string,
+    borderWidth: PropTypes.number,
+    borderColor: PropTypes.string,
+    titleColor: PropTypes.string,
+    buttonTextColor: PropTypes.string
+  }
+
+  render() {
+    const {
+      title,
+      img,
+      buttonText,
+      size,
+      style,
+      borderRadius,
+      theme,
+      borderWidth,
+      borderColor,
+      titleColor,
+      buttonTextColor
+    } = this.props
+
+    return (
+      <div style={style}>
+        <img
+          className={classnames({
+            [`${styles.img}`]: true,
+            [`${styles.dark}`]: theme === undefined || 'dark',
+            [`${styles.light}`]: theme === 'light'
+          })}
+          style={{'borderRadius': `${borderRadius}px`, 'border': `${borderWidth}px solid ${borderColor}`}}
+          width={size}
+          height={size}
+          src={img}
+          alt='splash-screen'
+          draggable={false}
+        />
+        <p style={{'color': `${titleColor}`}} className={classnames({
+          [`${styles.title}`]: true,
+          [`${styles.dark}`]: theme === undefined || 'dark',
+          [`${styles.light}`]: theme === 'light'
+        })}>{title}</p>
+        <p style={{'color': `${buttonTextColor}`}} className={classnames({
+          [`${styles.buttonText}`]: true,
+          [`${styles.dark}`]: theme === undefined || 'dark',
+          [`${styles.light}`]: theme === 'light'
+        })}>{buttonText}</p>
+      </div>
+    )
+  }
+}
+
+export default SplashScreen
