b3ec4477a73238ec27bf9c7b4dc87ae719ad6af7
oahmaro <osamaahmaro@gmail.com>
2018-08-26 05:30:34 +0300

    refactor: Change to object that contain colors for both themes

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,20 +1,52 @@
-export const keyColor = '#26AA91'
-
-export const black1 = '#1D1F23'
-export const black2 = '#212326'
-export const black3 = '#26292B'
-export const black4 = '#111213'
-export const black5 = '#191919'
-
-export const grey1 = '#24292E'
-export const grey2 = '#2A2F33'
-export const grey3 = '#31363A'
-export const grey4 = '#313B43'
-
-export const white1 = '#FFFFFF'
-export const white2 = '#CCCCCB'
-export const white3 = '#808080'
-export const white4 = '#515151'
-
-export const red = '#9E2626'
-export const orange = '#E2C08C'
+export const colors = {
+  dark: {
+    text: {
+      primary: '#cccccb',
+      secondary: '#808080',
+      disabled: '#515151',
+      other: '#9e9d9e'
+    },
+    state: {
+      key: '#26AA91',
+      success: '',
+      warning: '#E2C08C',
+      error: '#9E2626'
+    },
+    ui: {
+      fill1: '#111213',
+      fill2: '#191919',
+      fill3: '#1D1F23',
+      fill4: '#212326',
+      fill5: '#26292B',
+      fill6: '#24292E',
+      fill7: '#2A2F33',
+      fill8: '#31363A',
+      fill9: '#313B43'
+    }
+  },
+  light: {
+    text: {
+      primary: '',
+      secondary: '',
+      disabled: '',
+      other: ''
+    },
+    state: {
+      key: '',
+      success: '',
+      warning: '',
+      error: ''
+    },
+    ui: {
+      fill1: '',
+      fill2: '',
+      fill3: '',
+      fill4: '',
+      fill5: '',
+      fill6: '',
+      fill7: '',
+      fill8: '',
+      fill9: ''
+    }
+  }
+}
